24b301eb42d00095f0604a245f8e4bc58ef158fa
spk57 <3463852+spk57@users.noreply.github.com>
2024-12-26 11:25:06 -0500

    Debugged splitting segments

--- src/SGLM.jl
+++ src/SGLM.jl
@@ -8,18 +8,36 @@ module SGLM
 export sglm
 
 using GLM
-using Tables
+using DataFrames
+using StatsBase
+using StatsModels
 
-"Call glm for each segment.  Returns a collection of glm results"
-function sglm(formula, data, family, link, segments=1)
-  rows=data.rows()
+"""Call glm for each segment.  Returns a collection of glm results. </br>"""
+#function sglm(formula::FormulaTerm, data::DataFrame, segments=1, family, link, segments=1)
+#  [lm(formula, data[getRange(s), :]) for s in 1:segments]
+#end
+
+"""
+Segment an array into <segments> evenly spaced segments.  
+Returns a collection of <segments> views
+Note: Non evenly divisible rows are included in the last segment
+"""
+function segment(a::Array, segments)
+  rows=size(a,1)
   (segLen, rem)=divrem(rows, segments)
-  glm(formula, data, family, link) 
+  firstRow(s)=1+segLen*(s-1)
+  lastRow(s)=segLen*s
+  getRange(s)= s == segments ? range(firstRow(s), lastRow(s)+rem) : range(firstRow(s), lastRow(s)) 
+  [view(a, getRange(n), : ) for n in 1:segments]
 end
 
-"Call glm for each segment.  Returns a collection of glm results"
-#function sglm(X, y, family, link, segments=1)
-  
-#end
+"""Call lm for each segment.  Returns a collection of glm results
+ CX and Cy are collections of matrices of the independant and dependant variables
+"""
+function slm(CX::Vector, Cy::Vector, wts=wts)
+  for r in 1:size(CX,1)
+    fit(LinearModel, CX[r,:], y[r,:])
+  end
+end
 
 end # module SGLM
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,27 @@ using SGLM
 using GLM
 using Test
 using DataFrames
-
+using MarketData
+using Dates
 
 ## Formaldehyde data from the R Datasets package
 form = DataFrame([[0.1,0.3,0.5,0.6,0.7,0.9],[0.086,0.269,0.446,0.538,0.626,0.782]],
     [:Carb, :OptDen])
 
-@testset "Base tests" begin
+@testset "GLM exists test" begin
   Σ = [6.136653061224592e-05 -9.464489795918525e-05
       -9.464489795918525e-05 1.831836734693908e-04]
   lm1 = GLM.fit(LinearModel, @formula(OptDen ~ Carb), form)
   #Not retesting GLM, just making sure something comes back from GLM
   @test isa(lm1, StatsModels.TableRegressionModel)
+end
+
+@testset "Test Segmentation" begin
+  cld=DataFrame(cl)
+  cld.ts=Dates.value.(cld.timestamp)
+  cld2=select!(cld, Not([:timestamp]))
+  clm=Matrix(cld2)
+  s3=segment(clm, 3)
+  @test size(s3, 1) == 3
+  @test size(s3[1]) == (166,2)
 end
